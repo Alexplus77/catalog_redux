7a37da52506e8592c03800b38f6cf7716671b508
alexs <alexplus77@mail.ru>
2022-02-08 21:09:56 +0200

    commit

--- src/components/products_list/Products_List.jsx
+++ src/components/products_list/Products_List.jsx
@@ -9,16 +9,22 @@ const Products_List = () => {
     <div className="container-productPage">
       <h1>Приглядитесь к этим товарам</h1>
       <div className="productList-container">
-        {products.map(({ name, price, id, image, discount, priceDiscount }) => (
-          <Product_Item
-            key={id}
-            name={name}
-            price={price}
-            image={image}
-            discount={discount}
-            priceDiscount={priceDiscount}
-          />
-        ))}
+        {products.length ? (
+          products.map(
+            ({ name, price, id, image, discount, priceDiscount }) => (
+              <Product_Item
+                key={id}
+                name={name}
+                price={price}
+                image={image}
+                discount={discount}
+                priceDiscount={priceDiscount}
+              />
+            )
+          )
+        ) : (
+          <h3 className="text-noList">Пока нет товаров в списке</h3>
+        )}
       </div>
     </div>
   );
